c73b089d4fb9706212fdf991579faee2abb3ba54
kiganoakuma <daltonhberg@icloud.com>
2024-12-04 15:59:11 -0800

    little progross committing for file additions and ff

--- main.py
+++ main.py
@@ -6,13 +6,17 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
-        pygame.Surface.fill(screen, "black")
-        pygame.display.flip()
-
+        print("frame")
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        pygame.Surface.fill(screen, "black")
+        pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
